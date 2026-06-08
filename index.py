3e3e293fdc2689f6db79c7191b94b5aeff63524d
Kirti Garg <kirti.growingg@gmail.com>
2026-06-08 22:02:23 +0530

    index updeated

--- index.py
+++ index.py
@@ -5,8 +5,5 @@ print("changes in github")
 print("new rebase")
 print("product ")
 print("new changes")
-<<<<<<< HEAD
 print("buy now")
-=======
-print("add food")
->>>>>>> 5badb7e (update add)
+print("squash")
